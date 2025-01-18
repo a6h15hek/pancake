39b8e80bb8f8d5f53196b2580d8254eb0116d11d
Abhishek M. Yadav <a6h15hek@outlook.com>
2025-01-18 16:51:06 +0530

    Functions added to sync, list, open and start and build

--- cmd/project.go
+++ cmd/project.go
@@ -17,6 +17,8 @@ package cmd
 
 import (
 	"fmt"
+	"os/exec"
+	"path/filepath"
 
 	"github.com/a6h15hek/pancake/utils"
 	"github.com/spf13/cobra"
@@ -27,19 +29,11 @@ var projectCmd = &cobra.Command{
 	Use:     "project",
 	Aliases: []string{"p"},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(utils.NotImplemented)
+		listProjects()
 	},
 }
 
 var config utils.Config
-
-func addProject(args []string) {
-	fmt.Println(utils.NotImplemented)
-}
-
-func removeProject(args []string) {
-	fmt.Println(utils.NotImplemented)
-}
 
 func listProjects() {
 	config = *utils.GetConfig()
@@ -48,20 +42,142 @@ func listProjects() {
 	}
 }
 
+func syncSingleProject(projectName string) {
+	project, exists := config.Projects[projectName]
+	if !exists {
+		fmt.Printf("Project %s not found in configuration.\n", projectName)
+		return
+	}
+
+	projectPath := filepath.Join(config.Home, projectName)
+	gitDirPath := filepath.Join(projectPath, ".git")
+
+	projectExists := utils.CheckExists(projectPath)
+	gitExists := utils.CheckExists(gitDirPath)
+
+	if !projectExists || !gitExists {
+		utils.CloneRepository(projectPath, project.RemoteSSHURL)
+	} else {
+		utils.PullChanges(projectPath)
+	}
+	fmt.Printf("Synchronized project %s successfully.\n", projectName)
+}
+
 func syncProjects(args []string) {
-	fmt.Println(utils.NotImplemented)
+	config = *utils.GetConfig()
+	if len(args) == 0 {
+		if utils.ConfirmAction("sync") {
+			for projectName := range config.Projects {
+				syncSingleProject(projectName)
+			}
+		} else {
+			fmt.Println("Sync canceled.")
+		}
+	} else {
+		syncSingleProject(args[0])
+	}
 }
 
 func openProject(args []string) {
-	fmt.Println(utils.NotImplemented)
+	config = *utils.GetConfig()
+	path := config.Home
+	if len(args) > 0 {
+		path = filepath.Join(config.Home, args[0])
+	}
+
+	cmd := exec.Command(config.CodeEditor, path)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Error opening project: %v\n", err)
+	} else {
+		fmt.Printf("Opened project at %s\n", path)
+	}
+}
+
+func buildSingleProject(projectName string) {
+	project, exists := config.Projects[projectName]
+	if !exists {
+		fmt.Printf("Project %s not found in configuration.\n", projectName)
+		return
+	}
+
+	projectPath := filepath.Join(config.Home, projectName)
+	if !utils.CheckExists(projectPath) {
+		fmt.Printf("Project path %s does not exist.\n", projectPath)
+		return
+	}
+
+	if project.Build == "" {
+		fmt.Println("Build command not specified in the configuration.")
+		return
+	}
+
+	cmd := exec.Command("sh", "-c", project.Build)
+	cmd.Dir = projectPath
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Error building project %v: %v\n", projectName, err)
+	} else {
+		fmt.Printf("Built project %s successfully.\n", projectName)
+	}
 }
 
 func buildProject(args []string) {
-	fmt.Println(utils.NotImplemented)
+	config = *utils.GetConfig()
+	if len(args) == 0 {
+		if utils.ConfirmAction("build") {
+			for projectName := range config.Projects {
+				buildSingleProject(projectName)
+			}
+		} else {
+			fmt.Println("Build canceled.")
+		}
+	} else {
+		buildSingleProject(args[0])
+	}
+}
+
+func startSingleProject(projectName string) {
+	project, exists := config.Projects[projectName]
+	if !exists {
+		fmt.Printf("Project %s not found in configuration.\n", projectName)
+		return
+	}
+
+	projectPath := filepath.Join(config.Home, projectName)
+	if !utils.CheckExists(projectPath) {
+		fmt.Printf("Project path %s does not exist.\n", projectPath)
+		return
+	}
+
+	if project.Start == "" {
+		fmt.Println("Start command not specified in the configuration.")
+		return
+	}
+
+	cmd := exec.Command("sh", "-c", project.Start)
+	cmd.Dir = projectPath
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Error starting project %v: %v\n", projectName, err)
+	} else {
+		fmt.Printf("Started project %s successfully.\n", projectName)
+	}
 }
 
 func startProject(args []string) {
-	fmt.Println(utils.NotImplemented)
+	config = *utils.GetConfig()
+	if len(args) == 0 {
+		if utils.ConfirmAction("start") {
+			for projectName := range config.Projects {
+				startSingleProject(projectName)
+			}
+		} else {
+			fmt.Println("Start canceled.")
+		}
+	} else {
+		startSingleProject(args[0])
+	}
 }
 
 func stopProject(args []string) {
@@ -76,8 +192,6 @@ func init() {
 	rootCmd.AddCommand(projectCmd)
 
 	projectCmd.AddCommand(
-		&cobra.Command{Use: "add", Run: func(cmd *cobra.Command, args []string) { addProject(args) }},
-		&cobra.Command{Use: "remove", Run: func(cmd *cobra.Command, args []string) { removeProject(args) }},
 		&cobra.Command{Use: "list", Run: func(cmd *cobra.Command, args []string) { listProjects() }},
 		&cobra.Command{Use: "sync", Run: func(cmd *cobra.Command, args []string) { syncProjects(args) }},
 		&cobra.Command{Use: "open", Run: func(cmd *cobra.Command, args []string) { openProject(args) }},
