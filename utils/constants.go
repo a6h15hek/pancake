ffcafa4b3f433da4d5151173c41c94cff2eb04c3
Abhishek M. Yadav <a6h15hek@outlook.com>
2025-02-02 13:46:13 +0530

    version updated

--- utils/constants.go
+++ utils/constants.go
@@ -27,7 +27,7 @@ package utils
 
 const (
 	AppName         = "Pancake"
-	Version         = "v1.0.0"
+	Version         = "v1.1.0"
 	Description     = "A tool to streamline project management workflow."
 	LongDescription = `Example Usage:
   pancake list 
